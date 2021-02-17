d095f3e38fee440141e0b2662c962cc3ea3ba78b
INMihaylov19 <INMihaylov19@codingburgas.bg>
2021-02-17 22:37:24 +0200

    Make a prototype wizard

--- Magic Wizards/Magic Wizards/Main.cpp
+++ Magic Wizards/Magic Wizards/Main.cpp
@@ -1,8 +1,50 @@
 #include<iostream>
+#include<string>
+#include<iomanip>
 
 using namespace std;
 
+string playerName;
+
+void introduction()
+{
+                                        
+    cout << "                    &&" << endl;
+    cout << "                   %  %" << endl;
+    cout << "                  %    %" << endl;
+    cout << "                 %   %%%%" << endl;
+    cout << "                %#&%%%   %" << endl;
+    cout << "              %    %& %*(&" << endl;
+    cout << "              %  *   &&    *" << endl;
+    cout << "             %  &.##//   %#&." << endl;
+    cout << "            % .%/  .%( %#   %" << endl;
+    cout << "        .%(&  &%%(%*&     & %& %/%." << endl;
+    cout << "       %  #*  #&&%%&%%%&%&#   %. #/" << endl;
+    cout << "     %*%#    ,%  ,%   % *%   /.%.%" << endl;
+    cout << "     %  %         .%   #.      %  %" << endl;
+    cout << "      (%           %   ,#       &." << endl;
+    cout << "      % &,         %    %        %" << endl;
+    cout << "     %  ,%/#       %    %    ./(%,#" << endl;
+    cout << "    %      //      &   %  %%%#    .*" << endl;
+    cout << "   %           %%%%#%%%.           &/" << endl;
+    cout << "  &                                ,%" << endl;
+    cout << "  %                 #%,          .  %" << endl;
+    cout << "  %%%.                           *," << endl;
+    cout << "     %                           * (" << endl;
+    cout << "     %  .                         &" << endl;
+    cout << "        %   /          &    %&..#%" << endl;
+    cout << "            %    %    ,(%%%" << endl;
+    cout << "              *    *. " << endl;
+    cout << "==========================================" << endl;
+    cout << "Hello, my name is Stringgar the Wizard." << endl;
+    cout << "Today I'm going to teach you some things about strings." << endl;
+    cout << "First, what is your name." << endl;
+    cout << "My name is ";
+    getline(cin, playerName);
+    cout << "Okay, " << playerName << " let's start." << endl;
+    cout << "==========================================" << endl;                         
+}
 int main()
 {
-
+    introduction();
 }
